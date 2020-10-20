28a6986184a0a2ffc1ee785e747d9d6465fb1e84
Zachary Reiss <zachary.reiss123@gmail.com>
2020-10-20 15:41:14 -0600

    Starter code for lab 8

---
+++ Lab08/Lab08Tests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab08
+{
+    public class Lab08Tests
+    {
+        [TestCase(12.123537, 12.123536, ExpectedResult = true)]
+        [TestCase(1, .999999, ExpectedResult = true)]
+        [TestCase(12, 13, ExpectedResult = false)]
+        [TestCase(1, 10, ExpectedResult = false)]
+        public bool GivenTwoDoubles_ReturnTrueIfClose(double num1, double num2) => Program.isClose(num1, num2);
+
+        [TestCase(1, ExpectedResult = 2.54)]
+        [TestCase(6, ExpectedResult = 15.24)]
+        [TestCase(24, ExpectedResult = 60.96)]
+        [TestCase(1234, ExpectedResult = 3134.36)]
+        public double GivenInchecs_ReturnsConversionToCentimeters(double inches) => Program.InchesToCentimeters(inches);
+
+        [TestCase(1, ExpectedResult = .9144)]
+        [TestCase(6, ExpectedResult = 5.4864)]
+        [TestCase(24, ExpectedResult = 21.9456)]
+        [TestCase(1234, ExpectedResult = 1128.3696)]
+        public double GivenYards_ReturnsConversionToMeters(double yards) => Program.YardsToMeters(yards);
+
+        [TestCase(1, ExpectedResult = 1.609344)]
+        [TestCase(6, ExpectedResult = 9.656064)]
+        [TestCase(24, ExpectedResult = 38.624256)]
+        [TestCase(100, ExpectedResult = 160.9344)]
+        public double GivenMiles_ReturnsConversionToKilometers(double miles) => Program.MilesToKilometers(miles);
+    }
+}
---
+++ Lab08/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lab08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This will be your driving function that puts everything together
+        }
+
+        //make a function called isCLose that takes in two doubles and returns a bool
+        //make a function called InchesToCentimeters that takes a double and returns a double
+        //make a function called YardsToMeters that takes in a double and returns a double
+        //make a funciton called milesToKilometers that takes in a double and returns a double
+        //make a function called GetNumberFromUser that takes in a message and returns the value the user inputs
+        //make a function called DisplayNumberToConsole that takes in a double(the result) AND an output message and writes them out to the console
+    }
+}
